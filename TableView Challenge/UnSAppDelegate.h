8f1980e93b24fc36d729ec134310392f6d161ad0
hajjboy95 <gizmo-boy95@hotmail.com>
2014-05-29 15:59:50 +0100

    Initial Commit

---
+++ TableView Challenge/UnSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UnSAppDelegate.h
+//  TableView Challenge
+//
+//  Created by Ismail El-Habbash on 29/05/2014.
+//  Copyright (c) 2014 Ismail El-Habbash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UnSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableView Challenge/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableView Challenge
+//
+//  Created by Ismail El-Habbash on 29/05/2014.
+//  Copyright (c) 2014 Ismail El-Habbash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UnSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UnSAppDelegate class]));
+    }
+}
